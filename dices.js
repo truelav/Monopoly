f744a0d0cbd809cfb34b98b65be2ff834e3c4244
truelav <madan.serghei@yahoo.com>
2018-07-11 15:27:43 -0700

    working on moving players relic

--- dices.js
+++ dices.js
@@ -1,38 +1,43 @@
 let die1;
 let die2;
+let sumDices;
 
 $(document).ready(function() {
     $( ".roll-dice" ).click(function() {
         rollDice();
+        sumDices = die1 + die2;
+        //$(".numbers").css("display", "none");
+    
         if( die1 === 1 ) {
             $("#number-11").css("display", "block")
-          } else if ( die1 === 2 ) {
+            } else if ( die1 === 2 ) {
             $("#number-21, #number-31").css("display", "block")
-          } else if ( die1 === 3) {
-             $("#number-11, #number-41, #number-51").css("display", "block")
-          }  else if ( die1 === 4) {
-             $("#number-41, #number-61, #number-51, #number-71").css("display", "block")
-          }   else if ( die1 === 5) {
-             $("#number-11, #number-41, #number-61, #number-51, #number-71").css("display", "block")
-          }   else if ( die1 === 6) {
-             $("#number-21, #number-31, #number-41, #number-61, #number-51, #number-71").css("display", "block")
-          };
-          
-          if( die2 === 1 ) {
+            } else if ( die1 === 3) {
+                $("#number-11, #number-41, #number-51").css("display", "block")
+            }  else if ( die1 === 4) {
+                $("#number-41, #number-61, #number-51, #number-71").css("display", "block")
+            }   else if ( die1 === 5) {
+                $("#number-11, #number-41, #number-61, #number-51, #number-71").css("display", "block")
+            }   else if ( die1 === 6) {
+                $("#number-21, #number-31, #number-41, #number-61, #number-51, #number-71").css("display", "block")
+            };
+            
+            if( die2 === 1 ) {
             $("#number-12").css("display", "block")
-          } else if ( die2 === 2 ) {
+            } else if ( die2 === 2 ) {
             $("#number-22, #number-32").css("display", "block")
-          } else if ( die2 === 3) {
-             $("#number-12, #number-42, #number-52").css("display", "block")
-          }  else if ( die2 === 4) {
-             $("#number-42, #number-62, #number-52, #number-72").css("display", "block")
-          }   else if ( die2 === 5) {
-             $("#number-12, #number-42, #number-62, #number-52, #number-72").css("display", "block")
-          }   else if ( die2 === 6) {
-             $("#number-22, #number-32, #number-42, #number-62, #number-52, #number-72").css("display", "block")
-          };
-          
-          alert("" + die1 + " " + die2);
+            } else if ( die2 === 3) {
+                $("#number-12, #number-42, #number-52").css("display", "block")
+            }  else if ( die2 === 4) {
+                $("#number-42, #number-62, #number-52, #number-72").css("display", "block")
+            }   else if ( die2 === 5) {
+                $("#number-12, #number-42, #number-62, #number-52, #number-72").css("display", "block")
+            }   else if ( die2 === 6) {
+                $("#number-22, #number-32, #number-42, #number-62, #number-52, #number-72").css("display", "block")
+            };
+            
+
+        alert("" + die1 + " " + die2);
     });
 
 });
