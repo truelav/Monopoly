5b8f06ec2ad50ae705f60aa0f04e4702dc59c447
truelav <madan.serghei@yahoo.com>
2018-07-17 18:32:52 -0700

    working on the game updates functionality

--- dices.js
+++ dices.js
@@ -56,7 +56,11 @@ $(document).ready(function() {
 
         $(".player1-piece").detach().appendTo(`#cell-${player.position}`);    
         alert("" + die1 + " " + die2);
+
+        $(".game-updates").append( `<p>${board[player.position].name}</p>` )
     });
+
+    //$(".game-updates").append( `<p>${board[player.position].name}</p>` )
 
 });
 
@@ -67,6 +71,216 @@ const rollDice = function () {
 }
 
 
-
+const board  =  [{
+    name: "Go"
+  },{
+    name: "Old Kent Road",
+    price: 60,
+    rental: [2, 10, 30, 90, 160, 250],
+    color: "brown",
+    housePrice: 50,
+    type: "property"
+  },{
+    name: "Community Chest"
+  },{
+    name: "Whitechapel Road",
+    price: 60,
+    rental: [4, 20, 60, 180, 360, 450],
+    color: "brown",
+    housePrice: 50,
+    type: "property"
+  },{
+    name: "Income Tax",
+    type: "tax",
+    amount: 200
+  },{
+    name: "Kings Cross Station",
+    price: 200,
+    rental: [25, 50, 100, 200],
+    type: "station"
+  },{
+    name: "The Angel, Islington",
+    price: 100,
+    rental: [6, 30, 90, 270, 400, 550],
+    color: "lightBlue",
+    housePrice: 50,
+    type: "property"
+  },{
+    name: "Chance"
+  },{
+    name: "Euston Road",
+    price: 100,
+    rental: [6, 30, 90, 270, 400, 550],
+    color: "lightBlue",
+    housePrice: 50,
+    type: "property"
+  },{
+    name: "Pentonville Road",
+    price: 120,
+    rental: [8, 40, 100, 300, 450, 600],
+    color: "lightBlue",
+    housePrice: 50,
+    type: "property"
+  },{
+    name: "Jail"
+  },{
+    name: "Pall Mall",
+    price: 140,
+    rental: [10, 50, 150, 450, 625, 750],
+    color: "pink",
+    housePrice: 100,
+    type: "property"
+  },{
+    name: "Electric Company",
+    price: 150,
+    type: "utility"
+  },{
+    name: "Whitehall",
+    price: 140,
+    rental: [10, 50, 150, 450, 625, 750],
+    color: "pink",
+    housePrice: 100,
+    type: "property"
+  },{
+    name: "Northumberland Avenue",
+    price: 160,
+    rental: [12, 60, 180, 500, 700, 900],
+    color: "pink",
+    housePrice: 100,
+    type: "property"
+  },{
+    name: "Marylebone Station",
+    price: 200,
+    rental: [25, 50, 100, 200],
+    type: "station"
+  },{
+    name: "Bow Steet",
+    price: 180,
+    rental: [14, 70, 200, 550, 750, 950],
+    color: "orange",
+    housePrice: 100,
+    type: "property"
+  },{
+    name: "Community Chest"
+  },{
+    name: "Marlborough Steet",
+    price: 180,
+    rental: [14, 70, 200, 550, 750, 950],
+    color: "orange",
+    housePrice: 100,
+    type: "property"
+  },{
+    name: "Vine Steet",
+    price: 200,
+    rental: [16, 80, 220, 600, 800, 1000],
+    color: "orange",
+    housePrice: 100,
+    type: "property"
+  },{
+    name: "Free Parking"
+  },{
+    name: "The Strand",
+    price: 220,
+    rental: [18, 90, 250, 700, 875, 1050],
+    color: "red",
+    housePrice: 150,
+    type: "property"
+  },{
+    name: "Chance"
+  },{
+    name: "Fleet Street",
+    price: 220,
+    rental: [18, 90, 250, 700, 875, 1050],
+    color: "red",
+    housePrice: 150,
+    type: "property"
+  },{
+    name: "Trafalgar Square",
+    price: 240,
+    rental: [20, 100, 300, 750, 925, 1100],
+    color: "red",
+    housePrice: 150,
+    type: "property"
+  },{
+    name: "Fenchurch Street Station",
+    price: 200,
+    rental: [25, 50, 100, 200],
+    type: "station"
+  },{
+    name: "Leicester Square",
+    price: 260,
+    rental: [22, 110, 330, 800, 975, 1150],
+    color: "yellow",
+    housePrice: 150,
+    type: "property"
+  },{
+    name: "Water Works",
+    price: 150,
+    type: "utility"
+  },{
+    name: "Coventry Street",
+    price: 260,
+    rental: [22, 110, 330, 800, 975, 1150],
+    color: "yellow",
+    housePrice: 150,
+    type: "property"
+  },{
+    name: "Piccadilly",
+    price: 280,
+    rental: [22, 120, 360, 850, 1025, 1200],
+    color: "yellow",
+    housePrice: 140,
+    type: "property"
+  },{
+    name: "Go To Jail",
+  },{
+    name: "Regent Street",
+    price: 300,
+    rental: [26, 130, 390, 900, 1100, 1275],
+    color: "green",
+    housePrice: 200,
+    type: "property"
+  },{
+    name: "Oxford Street",
+    price: 300,
+    rental: [26, 130, 390, 900, 1100, 1275],
+    color: "green",
+    housePrice: 200,
+    type: "property"
+  },{
+    name: "Comminity Chest"
+  },{
+    name: "Bond Street",
+    price: 320,
+    rental: [28, 150, 450, 1000, 1200, 1400],
+    color: "green",
+    housePrice: 200,
+    type: "property"
+  },{
+    name: "Liverpool Street Station",
+    price: 200,
+    rental: [25, 50, 100, 200],
+    type: "station"
+  },{
+    name: "Chance"
+  },{
+    name: "Park Lane",
+    price: 350,
+    rental: [35, 175, 500, 1100, 1300, 1500],
+    color: "darkBlue",
+    housePrice: 200,
+    type: "property"
+  },{
+    name: "Super Tax",
+    type: "tax",
+    amount: 100
+  },{
+    name: "Mayfair",
+    price: 400,
+    rental: [50, 200, 600, 1400, 1700, 2000],
+    color: "darkBlue",
+    housePrice: 200,
+    type: "property"
+  }]
 
 
