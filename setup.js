efb3bd6b4d9062b7349b92bfeb776bd1f114a87c
truelav <madan.serghei@yahoo.com>
2018-07-24 12:12:12 -0700

    need to grab players info and initiate the game, select player symbol

--- setup.js
+++ setup.js
@@ -1,25 +1,35 @@
 $(document).ready(function(){
 
     
-
     $('#player-number').change(function() {
         var numberOfPlayers = $("#player-number").val();
-        // for (var i = 1; i <= numberOfPlayers; i++){
-            if (numberOfPlayers === '2'){
-              $( `#player-${1}-input, #player-${2}-input`).css("display", "block");
-              $(`#player-${3}-input, #player-${4}-input`).css("display", "none");
-            } else if (numberOfPlayers === '3'){
-                $(`#player-${3}-input, #player-${1}-input, #player-${2}-input`).css("display", "block");
-                $(`#player-${4}-input`).css("display", "none");
-            } else if (numberOfPlayers === '4'){
-                $(`#player-${3}-input, #player-${1}-input, #player-${2}-input, #player-${4}-input`).css("display", "block");
-            }
-        // }
-        // $('#player-4-input').css("display", "none");
-        console.log($("#player-number").val())
+        if (numberOfPlayers === '2'){
+            $( `#player-${1}-input, #player-${2}-input`).css("display", "block");
+            $(`#player-${3}-input, #player-${4}-input`).css("display", "none");
+        } else if (numberOfPlayers === '3'){
+            $(`#player-${3}-input, #player-${1}-input, #player-${2}-input`).css("display", "block");
+            $(`#player-${4}-input`).css("display", "none");
+        } else if (numberOfPlayers === '4'){
+            $(`#player-${3}-input, #player-${1}-input, #player-${2}-input, #player-${4}-input`).css("display", "block");
+        }
     })
 
     $(".start-game").click(function(){
+        var numberOfPlayers = $("#player-number").val();
+        var players = [];
+        // for (var i = 1; i <= numberOfPlayers; i++){
+            
+        // }
+        var player1 = { 
+                name: $(`#player-1-name`).val(),
+                color: $(`#player-1-color`).val()
+                }
+
+        var player2 = { 
+            name: $(`#player-2-name`).val(),
+            color: $(`#player-2-color`).val()
+            }            
+        console.log(player1, player2)
         $("#setup").css("display", "none");
         $(".page-view").css("display", "block");
     })
