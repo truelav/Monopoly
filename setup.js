7221d390d3172a8d198e16b03260485cf7ddded6
truelav <madan.serghei@yahoo.com>
2018-07-24 12:00:19 -0700

    still working on adding player dynamic

--- setup.js
+++ setup.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
 
+    
+
+    $('#player-number').change(function() {
+        var numberOfPlayers = $("#player-number").val();
+        // for (var i = 1; i <= numberOfPlayers; i++){
+            if (numberOfPlayers === '2'){
+              $( `#player-${1}-input, #player-${2}-input`).css("display", "block");
+              $(`#player-${3}-input, #player-${4}-input`).css("display", "none");
+            } else if (numberOfPlayers === '3'){
+                $(`#player-${3}-input, #player-${1}-input, #player-${2}-input`).css("display", "block");
+                $(`#player-${4}-input`).css("display", "none");
+            } else if (numberOfPlayers === '4'){
+                $(`#player-${3}-input, #player-${1}-input, #player-${2}-input, #player-${4}-input`).css("display", "block");
+            }
+        // }
+        // $('#player-4-input').css("display", "none");
+        console.log($("#player-number").val())
+    })
+
     $(".start-game").click(function(){
         $("#setup").css("display", "none");
         $(".page-view").css("display", "block");
